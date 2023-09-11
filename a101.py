bfd969f56a0065bbfdb91f179c5e4c0ca9871864
SnehaMostra <75886121+SnehaMostra@users.noreply.github.com>
2023-09-11 16:45:01 +0530

    Add files via upload

---
+++ a101.py
@@ -0,0 +1,41 @@
+import random
+responce= input('press y to roll again')
+while (responce=='y'):
+    no= random.randint(1,6)
+    if(no==1):
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    elif(no==2):
+        print("[-----]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[-----]")
+    elif(no==3):
+        print("[-----]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[-----]")
+    elif(no==4):
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    elif(no==5):
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[  0  ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    elif(no==6):
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    responce= input('press y to roll again')
